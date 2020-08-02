53c74c0b1c03e34d1ab207851eb80a142610d90b
sinakhalili <khalili@sfu.ca>
2020-08-02 16:06:25 -0700

    Add a file picker dialog
    Using inheritance. This is a bit easier than composition.
    Reference: https://codeloop.org/how-to-browse-a-file-in-python-tkinter/

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
-top = tkinter.Tk()
-top.mainloop()
+
+class Root(Tk):
+    """
+    This is the root object, which inherits from TK
+
+    The benefit of inheritance is we can write:
+    self.button instead of self.root.button
+    """
+
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Wav compression analyzer")
+        self.minsize(640, 400)
+
+        self.labelFrame = ttk.LabelFrame(self, text="Open File")
+        self.labelFrame.grid(column=0, row=1, padx=20, pady=20)
+
+        self.button()
+
+    def button(self):
+        self.button = ttk.Button(
+            self.labelFrame, text="Browse A File", command=self.file_dialog
+        )
+        self.button.grid(column=1, row=1)
+
+    def file_dialog(self):
+        """
+        Opens a file dialog and has the user chose a file
+
+        This then sets some labels afterwards
+        """
+
+        self.filename = filedialog.askopenfilename(
+            initialdir="./", title="Select A File",
+        )
+        self.label = ttk.Label(self.labelFrame, text="")
+        self.label.grid(column=1, row=2)
+        self.label.configure(text=self.filename)
+
+
+if __name__ == "__main__":
+    root = Root()
+    root.mainloop()
